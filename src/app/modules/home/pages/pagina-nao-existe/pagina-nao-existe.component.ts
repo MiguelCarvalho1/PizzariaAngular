73e8b103a3dceac55c71c88ad3529becf414bb45
migue <miguelangelosilvacarvalho96@gmail.com>
2024-04-04 12:10:52 +0100

    initial project

---
+++ src/app/modules/home/pages/pagina-nao-existe/pagina-nao-existe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-nao-existe',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina-nao-existe.component.html',
+  styleUrl: './pagina-nao-existe.component.css'
+})
+export class PaginaNaoExisteComponent {
+
+}
